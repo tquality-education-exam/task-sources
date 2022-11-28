a4b168c1bc64fe6942862499ddea8bd954264f4a
v.popchenko <v.popchenko@qa-academy.by>
2022-11-28 23:16:18 +0300

    Added Content.java. Modified TestController.java - changed @RequestParam(...) string content/contentType to @RequestBody Content content

---
+++ union-common-persistence/src/main/java/com/a1qa/model/domain/Content.java
@@ -0,0 +1,23 @@
+package com.a1qa.model.domain;
+
+public class Content {
+    public String content;
+
+    public String contentType;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+}
